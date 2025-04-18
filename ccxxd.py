0c7c951d6da096a83a5fb95b692909bee5327c88
Craig Giddings <randomhero_345@hotmail.com>
2025-04-18 19:50:50 -0400

    Added a hexdump function with grouping

--- ccxxd.py
+++ ccxxd.py
@@ -1,12 +1,11 @@
-def main():
+import argparse
 
-    with open("files.tar", "rb") as file:
+def hexdump(source, group=2):
+    with open(source, "rb") as file:
         binary_data = file.read()
 
         # Each row contains 16 bytes of data
         size = 16
-        # Combine two bytes per row
-        step = 2
         
         # Iterate through the file contents
         for row in range(0, len(binary_data), size):
@@ -14,12 +13,14 @@ def main():
             print(f"{row:08X}:", end=" ")
             # Grab 16 bytes at a time
             hex_data = binary_data[row:row + size]
-            for i in range(0, len(hex_data), step):
-                # Print the hexadecimal values
-                print(f"{hex_data[i]:02x}{hex_data[i+1]:02x}", end=" ")
-            # Print the ASCII text at the end
+            print(" ".join([hex_data[i:i + group].hex() for i in range(0, len(hex_data), group)]), end=" ")
             print("".join([chr(i) if 32 <= i <= 126 else "." for i in hex_data]), end="")
             print("")
+
+def main():
+
+    hexdump("files.tar")
+    
 
 if __name__ == "__main__":
     main()
